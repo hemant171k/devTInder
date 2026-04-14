354aecbc98dbbf0faf8f7e7eedf57322effef0cb
Hemant Kumar <hemant171k@gmail.com>
2026-04-14 08:13:04 +0530

    working with routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,26 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(
+    "req params are ==>",
+    req.params.userId,
+    req.params.name,
+    req.params.password,
+  );
+  console.log("req queries are==>",req.query.city,req.query.state)
+  res.send("Get Method for user");
+});
+app.post("/user", (req, res) => {
+  res.send("Post Method for user here user data will be saved");
+});
+app.patch("/user", (req, res) => {
+  res.send("Patch Method for user here user data will be updated");
+});
+app.delete("/user", (req, res) => {
+  res.send("Delete Method for user here user will be deleted");
+});
+
 app.use((req, res) => {
   res.send("welcome here");
 });
