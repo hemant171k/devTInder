862035d49b2fe2e3ce9c91cb3675a90e88754a30
Hemant Kumar <hemant171k@gmail.com>
2026-04-15 07:05:09 +0530

    authorized admin using middleware and process to its route

--- src/app.js
+++ src/app.js
@@ -2,29 +2,23 @@ const express = require("express");
 
 const app = express();
 
+// Authorized admin login user usign middleware and then processed it to actual its respective route
+
+app.use("/admin", (req, res, next) => {
+  let token = "abcxyz"; //get token token firstly from req and then check it from db or session
+  if (token != "abcxyz") {
+    res.status(401).send("unauthrized access");
+  } else {
+    next();
+  }
+});
+app.get("/admin/getAllData", (req, res) => {
+  res.send("here get all user data");
+});
+app.get("/admin/deletUser", (req, res) => {
+  res.send("requested user deleted");
+});
+
 app.listen(3000, () => {
   console.log("server is listening on port 3000");
 });
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("first middleware");
-    next();
-  },
-  (req, res, next) => {
-    console.log("2nd middleware");
-    next();
-  },
-  (req, res, next) => {
-    console.log("third middleware");
-    next();
-  },
-  (req, res, next) => {
-    console.log("4th middleware");
-    next();
-  },
-  (req, res) => {
-    console.log("welcome to Route handler");
-    res.send("welcome to User");
-  },
-);
