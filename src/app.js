fe421c4f4949e2bc20a1e9affe7f564c5c644220
Hemant Kumar <hemant171k@gmail.com>
2026-04-15 07:37:19 +0530

    Error handling

--- src/app.js
+++ src/app.js
@@ -4,19 +4,28 @@ const app = express();
 
 // Authorized admin login user usign middleware and then processed it to actual its respective route
 
-app.use("/admin", (req, res, next) => {
-  let token = "abcxyz"; //get token token firstly from req and then check it from db or session
-  if (token != "abcxyz") {
-    res.status(401).send("unauthrized access");
-  } else {
-    next();
-  }
+app.use("/", (err, req, res, next) => {
+  // this function not gonna work for error handling becuase express follows its order to resolve route handling
+  res.status(500).send("Something went wrong");
 });
 app.get("/admin/getAllData", (req, res) => {
+  throw new Error("error occured");
   res.send("here get all user data");
 });
 app.get("/admin/deletUser", (req, res) => {
-  res.send("requested user deleted");
+  // error handling use try/catch
+  try {
+    res.send("requested user deleted");
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.use("/", (err, req, res, next) => {
+  // this function  gonna work for error and handles the error raised by /admin/getAllData
+  if (err) {
+    res.status(500).send(err);
+  }
 });
 
 app.listen(3000, () => {
